d1d3810810007289a9a9cae9f75f2b2fe20886d4
amatl <aimee.matland@gmail.com>
2014-11-25 20:58:45 -0600

    Created Quadrature Hybrid Class

--- QuadratureHybrid.m
+++ QuadratureHybrid.m
@@ -1,5 +1,28 @@
-z0 = 15;
-S = [0 (-1*1i/sqrt(2)) (-1/sqrt(2)) 0; (-1*1i/sqrt(2)) 0 0 (-1/sqrt(2)); (-1/sqrt(2)) 0 0 (-1*1i/sqrt(2)); 0 (-1/sqrt(2)) (-1*1i/sqrt(2)) 0];
-Z = s2z4(S);
-%find coupling
-C = 10*log(1/(1-(Z(0+1,1+1)/z0)^2));
+classdef QuadratureHybrid
+    methods (Access = public, Static)
+        function [width] = getWidth(substrateThickness, fabricationType)
+            switch fabricationType
+                case 'Micro'
+                    %test parameters
+                    z0 = 50;
+                    lambda_0 = 30;
+                    epsilon_r = 2.2;
+                    d = substrateThickness;
+                    %Give
+                    d = 0.158*10^(-2);
+
+                    S = [0 (-1*1i/sqrt(2)) (-1/sqrt(2)) 0; (-1*1i/sqrt(2)) 0 0 (-1/sqrt(2)); (-1/sqrt(2)) 0 0 (-1*1i/sqrt(2)); 0 (-1/sqrt(2)) (-1*1i/sqrt(2)) 0];
+                    Z = s2z4(S);
+
+                    %find coupling
+                    C = 10*log(1/(1-(Z(0+1,1+1)/z0)^2));
+                    lambda = lambda_0/sqrt(epsilon_r);
+
+                    %W for microstrip
+                    width = d*WDratio_g2(z0,epsilon_r);
+                case 'Strip'
+                case 'Coax'
+            end
+        end
+    end
+end
